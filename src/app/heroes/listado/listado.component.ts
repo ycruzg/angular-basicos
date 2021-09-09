02a3a25c82da946a6451641372d8bf9376d277ba
Yaimi Cruz <cruzyaimi@gmail.com>
2021-09-09 15:50:58 -0400

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string [] = ['Spiderman','Ironman','Hulk','Thor','Steve'];
+  
+  heroeBorrado: string = '' 
+  
+  borrarHeore(): void {
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+  
+}
